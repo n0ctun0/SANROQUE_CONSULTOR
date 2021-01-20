6edd617421f6b54d6864108dd001732ece1a494d
david <david.garcia2388@gmail.com>
2021-01-20 11:45:07 -0300

    Mejora para la adaptar el tamaño de la pantalla

--- app/src/main/java/com/example/sanroque_consultor/MainActivity.java
+++ app/src/main/java/com/example/sanroque_consultor/MainActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import static android.widget.Toast.LENGTH_LONG;
@@ -16,6 +19,7 @@ import static android.widget.Toast.LENGTH_LONG;
 public class MainActivity extends AppCompatActivity {
     Button iniciar;
     EditText ip,suc;
+    TextView version;
     SharedPreferences pref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +31,28 @@ public class MainActivity extends AppCompatActivity {
         ip = findViewById(R.id.edit_ip);
         suc = findViewById(R.id.edit_sucursal);
 
+
+        version= findViewById(R.id.txtversion);
+
+
         pref = getSharedPreferences("USUARIO", Context.MODE_PRIVATE);
         String ipp = pref.getString("IP", "200.40.253.210");
         String succ = pref.getString("SUC", "2");
 
         ip.setText(ipp);
         suc.setText(succ);
+
+        PackageInfo pinfo;
+
+        try {
+            pinfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            version.setText(pinfo.versionCode);
+
+
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
 
         iniciar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,5 +76,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
+
 
 }
